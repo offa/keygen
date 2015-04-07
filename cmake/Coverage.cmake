8d6f98f0df42faa0d9f7074a19eeb4e87a5c7c2f
offa <bm-dev@yandex.com>
2015-04-07 21:39:43 +0200

    Search gcov.

--- cmake/Coverage.cmake
+++ cmake/Coverage.cmake
@@ -2,7 +2,14 @@
 message(STATUS "Code coverage ${COVERAGE}")
 
 if( ${COVERAGE} )
+    find_program(GCOV_EXEC gcov)
+
+    if( NOT GCOV_EXEC )
+        message(FATAL_ERROR "Executable for gcov not found!")
+    endif()
+
     set(GCOV_FLAGS "-ftest-coverage -fprofile-arcs")
     set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} ${GCOV_FLAGS}")
     set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${GCOV_FLAGS}")
+
 endif()
