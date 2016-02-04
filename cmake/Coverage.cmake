0a4450ecaadae0f2ff0a1f082b31981147d3858f
offa <bm-dev@yandex.com>
2016-02-04 14:52:17 +0100

    Coverage generation updated.

--- cmake/Coverage.cmake
+++ cmake/Coverage.cmake
@@ -23,14 +23,16 @@ if( ${COVERAGE} )
 
     # Compiler flags
     set(COV_FLAGS "-ftest-coverage -fprofile-arcs")
-    set(CMAKE_C_FLAGS_DEBUG "${CMAKE_C_FLAGS_DEBUG} ${COV_FLAGS}")
-    set(CMAKE_CXX_FLAGS_DEBUG "${CMAKE_CXX_FLAGS_DEBUG} ${COV_FLAGS}")
+    set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} ${COV_FLAGS}")
+    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${COV_FLAGS}")
 
+
+    set(INPUT_DIR "${CMAKE_SOURCE_DIR}")
 
     # Make targets
     add_custom_target(coverage-clean ${CMAKE_COMMAND} -E make_directory ${COV_DIR}
                                     COMMAND ${LCOV}
-                                    --directory ${CMAKE_SOURCE_DIR}
+                                    --directory ${INPUT_DIR}
                                     --zerocounters
 
                                     WORKING_DIRECTORY ${COV_DIR}
@@ -41,7 +43,7 @@ if( ${COVERAGE} )
     add_custom_target(coverage ${CMAKE_COMMAND} -E make_directory ${COV_DIR}
                                     COMMAND ${LCOV}
                                     --capture
-                                    --directory ${CMAKE_SOURCE_DIR}
+                                    --directory ${INPUT_DIR}
                                     --output-file ${COV_FILE}
                                     --no-external
 
