46f06137680768b0b619ffc72d8f7150334333ca
offa <bm-dev@yandex.com>
2017-02-07 20:24:14 +0100

    Valgrind flag added.

--- cmake/Memcheck.cmake
+++ cmake/Memcheck.cmake
@@ -2,7 +2,12 @@
 find_program(VALGRIND valgrind DOC "Valgrind executable")
 
 if( VALGRIND )
-    set(VALGRIND_MEMCHECK_FLAGS --tool=memcheck --track-origins=yes --error-exitcode=1 --suppressions=${CMAKE_SOURCE_DIR}/cmake/valgrind.suppressions)
+    set(VALGRIND_MEMCHECK_FLAGS --tool=memcheck
+                                --track-origins=yes
+                                --leak-check=full
+                                --error-exitcode=1
+                                --suppressions=${CMAKE_SOURCE_DIR}/cmake/valgrind.suppressions
+                                )
     message(STATUS "Valgrind found")
 else()
     message(STATUS "Valgrind not found")
