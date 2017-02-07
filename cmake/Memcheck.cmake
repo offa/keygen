35d39b1d7774489ba0904b14ae5d337a3e26f5dd
offa <bm-dev@yandex.com>
2017-02-07 19:28:50 +0100

    Search and check valgrind.

---
+++ cmake/Memcheck.cmake
@@ -0,0 +1,10 @@
+
+find_program(VALGRIND valgrind DOC "Valgrind executable")
+
+if( VALGRIND )
+    set(VALGRIND_MEMCHECK_FLAGS --tool=memcheck --track-origins=yes --error-exitcode=1 --suppressions=${CMAKE_SOURCE_DIR}/cmake/valgrind.suppressions)
+    message(STATUS "Valgrind found")
+else()
+    message(STATUS "Valgrind not found")
+endif()
+
