1b70f22427d2e7584d93d53979d81d047f12bfbb
offa <bm-dev@yandex.com>
2017-09-12 12:12:34 +0200

    Installation of CppUTest master fixed.

--- script/ci_install.sh
+++ script/ci_install.sh
@@ -4,18 +4,16 @@ set -ex
 
 
 ## Install CppUTest
-CPPUTEST_VERSION=3.8
-CPPUTEST=cpputest-${CPPUTEST_VERSION}
-
+CPPUTEST_VERSION=master
 BUILD_FLAGS="-DC++11=OFF -DTESTS=OFF"
 
 if [[ "$CXX" == clang* ]]; then
     BUILD_FLAGS="$BUILD_FLAGS -DCMAKE_CXX_FLAGS=-stdlib=libc++" 
 fi
 
-wget https://github.com/cpputest/cpputest/releases/download/v${CPPUTEST_VERSION}/${CPPUTEST}.tar.gz
-tar -xzf ${CPPUTEST}.tar.gz
-pushd ${CPPUTEST}
+wget https://github.com/cpputest/cpputest/archive/${CPPUTEST_VERSION}.tar.gz
+tar -xzf ${CPPUTEST_VERSION}.tar.gz
+pushd cpputest-${CPPUTEST_VERSION}
 mkdir _build && cd _build
 cmake $BUILD_FLAGS ..
 make -j4 && sudo make install
