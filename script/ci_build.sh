ba1792c41466d36cb98b0f8a66f05e7048e62dd3
offa <bm-dev@yandex.com>
2018-01-08 15:57:05 +0100

    Sanitizers disabled on clang builds (#18); Valgrind is used instead on these builds.

--- script/ci_build.sh
+++ script/ci_build.sh
@@ -2,10 +2,13 @@
 
 set -ex
 
-
 if [[ "${CXX}" == clang* ]]
 then
     export CXXFLAGS="-stdlib=libc++"
+    # Clang's sanitizer conflict with CppUTest's Leakdetection (see #18)
+    # Instead of ASan / UBSan, Valgrind is used later in the build
+else
+    BUILD_FLAGS_SANITIZER="-DSANITIZER_ASAN=ON DSANITIZER_UBSAN=ON"
 fi
 
 
@@ -13,8 +16,14 @@ mkdir build && cd build
 
 cmake -DCMAKE_BUILD_TYPE=Debug \
         -DUNITTEST_VERBOSE=ON \
-        -DSANITIZER_ASAN=ON \
-        -DSANITIZER_UBSAN=ON \
+        ${BUILD_FLAGS_SANITIZER} \
         ..
 make all
 make unittest
+
+
+if [[ "${CXX}" == clang* ]]
+then
+    make memcheck
+    make memcheck-test
+fi
