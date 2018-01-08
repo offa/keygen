bd026d81ed179ee8843f51e38a56812135c5f3f7
offa <bm-dev@yandex.com>
2018-01-08 16:08:09 +0100

    Additional GCC build with Valgrind enabled added (#18). Refactoring of the CI script.

--- script/ci_build.sh
+++ script/ci_build.sh
@@ -2,13 +2,32 @@
 
 set -ex
 
+VALGRIND=false
+SANITIZER=true
+
+for arg in "$@"
+do
+    case "${arg}" in
+        -valgrind)
+            USE_VALGRIND=true
+            ;;
+    esac
+done
+
+
 if [[ "${CXX}" == clang* ]]
 then
     export CXXFLAGS="-stdlib=libc++"
+
     # Clang's sanitizer conflict with CppUTest's Leakdetection (see #18)
     # Instead of ASan / UBSan, Valgrind is used later in the build
-else
-    BUILD_FLAGS_SANITIZER="-DSANITIZER_ASAN=ON DSANITIZER_UBSAN=ON"
+    VALGRIND=true
+    SANITIZER=false
+fi
+
+if [[ "${SANITIZER}" == true ]]
+then
+    BUILD_FLAGS="-DSANITIZER_ASAN=ON DSANITIZER_UBSAN=ON"
 fi
 
 
@@ -16,13 +35,13 @@ mkdir build && cd build
 
 cmake -DCMAKE_BUILD_TYPE=Debug \
         -DUNITTEST_VERBOSE=ON \
-        ${BUILD_FLAGS_SANITIZER} \
+        ${BUILD_FLAGS} \
         ..
 make all
 make unittest
 
 
-if [[ "${CXX}" == clang* ]]
+if [[ "${VALGRIND}" == true ]]
 then
     make memcheck
     make memcheck-test
