6b2ae52f97fe932d8d130e9a3e382af3ca6c5564
offa <bm-dev@yandex.com>
2018-01-09 18:09:53 +0100

    Refactoring; Compiler options extracted.

---
+++ cmake/CompilerOptions.cmake
@@ -0,0 +1,12 @@
+
+add_compile_options(-Wall
+                    -Wextra
+                    -Werror
+                    -pedantic
+                    -Wshadow
+                    )
+
+set(CMAKE_C_STANDARD 11)
+set(CMAKE_C_STANDARD_REQUIRED ON)
+set(CMAKE_C_EXTENSIONS OFF)
+
