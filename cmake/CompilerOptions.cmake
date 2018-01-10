c1599d281de073631776a2bdef1c30e8b77ccf80
offa <bm-dev@yandex.com>
2018-01-10 13:40:58 +0100

    C and C++ options separated (on a directory basis). Oldstyle casts disallowed for C++.

--- cmake/CompilerOptions.cmake
+++ cmake/CompilerOptions.cmake
@@ -1,12 +1,26 @@
+set(COMMON_OPTIONS
+        -Wall
+        -Wextra
+        -Werror
+        -pedantic
+        -Wshadow
+        )
 
-add_compile_options(-Wall
-                    -Wextra
-                    -Werror
-                    -pedantic
-                    -Wshadow
-                    )
+set(C_OPTIONS )
+set(CPP_OPTIONS
+        -Wold-style-cast
+        )
+
 
 set(CMAKE_C_STANDARD 11)
 set(CMAKE_C_STANDARD_REQUIRED ON)
 set(CMAKE_C_EXTENSIONS OFF)
 
+
+function(enable_c_options)
+    add_compile_options(${C_OPTIONS})
+endfunction()
+
+function(enable_cpp_options)
+    add_compile_options(${CPP_OPTIONS})
+endfunction()
