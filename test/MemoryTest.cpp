b5d0db3ed2e79ab13b8e8776957ec6780a44c979
offa <bm-dev@yandex.com>
2018-01-11 18:41:19 +0100

    Refactoring.

--- test/MemoryTest.cpp
+++ test/MemoryTest.cpp
@@ -20,13 +20,25 @@
 
 #include "keygen/KeyGen.h"
 #include "TestUtil.h"
+#include <vector>
+#include <iterator>
 #include <string.h>
 #include <CppUTest/TestHarness.h>
 
 TEST_GROUP(MemoryTest)
 {
+    std::vector<std::uint8_t> createGuardedBuffer(std::size_t size) const
+    {
+        std::vector<std::uint8_t> buffer(size, 0x00);
+        buffer[0] = 0xCA;
+        buffer[1] = 0xFE;
+        buffer[size - 2] = 0xCA;
+        buffer[size - 1] = 0xFE;
+        return buffer;
+    }
+
     static constexpr std::size_t size{1000};
-    static constexpr std::size_t guardedSized{size + 4};
+    static constexpr std::size_t guardedSize{size + 4};
 };
 
 TEST(MemoryTest, testCleanUp)
@@ -46,25 +58,19 @@ TEST(MemoryTest, testCleanUp)
 
 TEST(MemoryTest, cleanUpBorderCheck)
 {
-    uint8_t* allocBuffer = allocate(guardedSized);
+    uint8_t* allocBuffer = allocate(guardedSize);
     uint8_t* buffer = allocBuffer + 2;
-    uint8_t expected[guardedSized];
-
-    memset(expected, 0, guardedSized);
-    expected[0] = 0xCA;
-    expected[1] = 0xFE;
-    expected[guardedSized - 2] = 0xCA;
-    expected[guardedSized - 1] = 0xFE;
+    const auto expected = createGuardedBuffer(guardedSize);
 
     const KeyGenError rtn = keygen_createKey(buffer, size, ASCII);
     CHECK_EQUAL(KG_ERR_SUCCESS, rtn);
 
     keygen_cleanBuffer(buffer, size);
-    MEMCMP_EQUAL(expected + 2, buffer, size);
+    MEMCMP_EQUAL(&(expected[2]), buffer, size);
     CHECK_EQUAL(0xCA, expected[0]);
     CHECK_EQUAL(0xFE, expected[1]);
-    CHECK_EQUAL(0xCA, expected[guardedSized - 2]);
-    CHECK_EQUAL(0xFE, expected[guardedSized - 1]);
+    CHECK_EQUAL(0xCA, expected[guardedSize - 2]);
+    CHECK_EQUAL(0xFE, expected[guardedSize - 1]);
 
     free(allocBuffer);
 }
@@ -82,28 +88,28 @@ TEST(MemoryTest, overlength)
 
 TEST(MemoryTest, overAndUnderflow)
 {
-    uint8_t* allocBuffer = allocate(guardedSized);
+    uint8_t* allocBuffer = allocate(guardedSize);
     uint8_t* buffer = allocBuffer + 2;
 
     allocBuffer[0] = 0xCA;
     allocBuffer[1] = 0xFE;
-    allocBuffer[guardedSized - 2] = 0xCA;
-    allocBuffer[guardedSized - 1] = 0xFE;
+    allocBuffer[guardedSize - 2] = 0xCA;
+    allocBuffer[guardedSize - 1] = 0xFE;
 
     const KeyGenError rtn = keygen_createKey(buffer, size, ASCII);
     CHECK_EQUAL(KG_ERR_SUCCESS, rtn);
 
     CHECK_EQUAL(0xCA, allocBuffer[0]);
     CHECK_EQUAL(0XFE, allocBuffer[1]);
-    CHECK_EQUAL(0xCA, allocBuffer[guardedSized - 2]);
-    CHECK_EQUAL(0xFE, allocBuffer[guardedSized - 1]);
+    CHECK_EQUAL(0xCA, allocBuffer[guardedSize - 2]);
+    CHECK_EQUAL(0xFE, allocBuffer[guardedSize - 1]);
 
     keygen_cleanBuffer(buffer, size);
 
     CHECK_EQUAL(0xCA, allocBuffer[0]);
     CHECK_EQUAL(0XFE, allocBuffer[1]);
-    CHECK_EQUAL(0xCA, allocBuffer[guardedSized - 2]);
-    CHECK_EQUAL(0xFE, allocBuffer[guardedSized - 1]);
+    CHECK_EQUAL(0xCA, allocBuffer[guardedSize - 2]);
+    CHECK_EQUAL(0xFE, allocBuffer[guardedSize - 1]);
 
     free(allocBuffer);
 }
