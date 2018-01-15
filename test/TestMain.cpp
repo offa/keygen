1f848e5f6e616f5b76de328e22236f16ceac106a
offa <bm-dev@yandex.com>
2018-01-15 13:38:38 +0100

    Trompeloeil integrated.

--- test/TestMain.cpp
+++ test/TestMain.cpp
@@ -20,3 +20,32 @@
 
 #define CATCH_CONFIG_MAIN
 #include <catch.hpp>
+#include <trompeloeil.hpp>
+
+
+namespace trompeloeil
+{
+    template <>
+    void reporter<specialized>::send(severity s, const char* file, unsigned long line, const char* msg)
+    {
+        std::ostringstream os;
+
+        if( line )
+        {
+            os << file << ':' << line << '\n';
+        }
+
+        os << msg;
+        const  auto failure = os.str();
+
+        if( s == severity::fatal )
+        {
+            FAIL(failure);
+        }
+        else
+        {
+            CAPTURE(failure);
+            CHECK(failure.empty());
+        }
+    }
+}
