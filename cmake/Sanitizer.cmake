8743494ffd6ad59c1378d66bfadea92e669fef6e
offa <bm-dev@yandex.com>
2018-01-19 10:27:42 +0100

    Sanitizer flags ported to new flag mechanism.

--- cmake/Sanitizer.cmake
+++ cmake/Sanitizer.cmake
@@ -3,7 +3,7 @@ add_library(build-libs INTERFACE)
 
 macro(enable_sanitizer san)
     set(SAN_FLAG "-fsanitize=${san}")
-    add_compile_options(${SAN_FLAG})
+    add_common_flags(${SAN_FLAG})
     target_link_libraries(build-libs INTERFACE ${SAN_FLAG})
 endmacro()
 
