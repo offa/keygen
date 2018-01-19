858b7bfbb90d89333934432f8612760ba83d5050
offa <bm-dev@yandex.com>
2018-01-19 10:15:20 +0100

    Compiler flags handling refactored.

--- cmake/CompilerOptions.cmake
+++ cmake/CompilerOptions.cmake
@@ -1,35 +1,35 @@
-set(flags_common
-        -Wall
-        -Wextra
-        -Werror
-        -pedantic
-        -Wshadow
-        )
 
-set(flags_c ${flags_common})
-set(flags_cpp ${flags_common}
-        -Wold-style-cast
-        )
+macro(add_common_flags)
+    set(flags_common ${flags_common} ${ARGN})
+endmacro()
+
+macro(add_c_flags)
+    set(flags_c ${flags_c} ${ARGN})
+endmacro()
+
+macro(add_cpp_flags)
+    set(flags_cpp ${flags_cpp} ${ARGN})
+endmacro()
 
 
 function(add_c_library name)
     add_library(${name} ${ARGN})
-    target_compile_options(${name} PRIVATE ${flags_c})
+    target_compile_options(${name} PRIVATE ${flags_common} ${flags_c})
 endfunction()
 
 function(add_c_executable name)
     add_executable(${name} ${ARGN})
-    target_compile_options(${name} PRIVATE ${flags_c})
+    target_compile_options(${name} PRIVATE ${flags_common} ${flags_c})
 endfunction()
 
 
 function(add_cpp_library name)
     add_library(${name} ${ARGN})
-    target_compile_options(${name} PRIVATE ${flags_cpp})
+    target_compile_options(${name} PRIVATE ${flags_common} ${flags_cpp})
 endfunction()
 
 function(add_cpp_executable name)
     add_executable(${name} ${ARGN})
-    target_compile_options(${name} PRIVATE ${flags_cpp})
+    target_compile_options(${name} PRIVATE ${flags_common} ${flags_cpp})
 endfunction()
 
