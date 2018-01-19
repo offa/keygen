4453d64ab8a6fe9229653257688fdcbbc8dbc36b
offa <bm-dev@yandex.com>
2018-01-19 09:44:43 +0100

    Variable renamed.

--- cmake/CompilerOptions.cmake
+++ cmake/CompilerOptions.cmake
@@ -1,4 +1,4 @@
-set(COMMON_FLAGS
+set(flags_common
         -Wall
         -Wextra
         -Werror
@@ -6,30 +6,30 @@ set(COMMON_FLAGS
         -Wshadow
         )
 
-set(C_FLAGS ${COMMON_FLAGS})
-set(CPP_FLAGS ${COMMON_FLAGS}
+set(flags_c ${flags_common})
+set(flags_cpp ${flags_common}
         -Wold-style-cast
         )
 
 
 function(add_c_library name)
     add_library(${name} ${ARGN})
-    target_compile_options(${name} PRIVATE ${C_FLAGS})
+    target_compile_options(${name} PRIVATE ${flags_c})
 endfunction()
 
 function(add_c_executable name)
     add_executable(${name} ${ARGN})
-    target_compile_options(${name} PRIVATE ${C_FLAGS})
+    target_compile_options(${name} PRIVATE ${flags_c})
 endfunction()
 
 
 function(add_cpp_library name)
     add_library(${name} ${ARGN})
-    target_compile_options(${name} PRIVATE ${CPP_FLAGS})
+    target_compile_options(${name} PRIVATE ${flags_cpp})
 endfunction()
 
 function(add_cpp_executable name)
     add_executable(${name} ${ARGN})
-    target_compile_options(${name} PRIVATE ${CPP_FLAGS})
+    target_compile_options(${name} PRIVATE ${flags_cpp})
 endfunction()
 
