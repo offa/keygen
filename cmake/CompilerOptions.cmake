cfcbc33721d4d09feaf6da9f5cbe6929a35a7daa
offa <bm-dev@yandex.com>
2018-01-19 09:42:30 +0100

    Setting of C and C++ standard moved to main CMakeLists.

--- cmake/CompilerOptions.cmake
+++ cmake/CompilerOptions.cmake
@@ -12,16 +12,6 @@ set(CPP_FLAGS ${COMMON_FLAGS}
         )
 
 
-set(CMAKE_C_STANDARD 11)
-set(CMAKE_C_STANDARD_REQUIRED ON)
-set(CMAKE_C_EXTENSIONS OFF)
-
-set(CMAKE_CXX_STANDARD 14)
-set(CMAKE_CXX_STANDARD_REQUIRED ON)
-set(CMAKE_CXX_EXTENSIONS OFF)
-
-
-
 function(add_c_library name)
     add_library(${name} ${ARGN})
     target_compile_options(${name} PRIVATE ${C_FLAGS})
